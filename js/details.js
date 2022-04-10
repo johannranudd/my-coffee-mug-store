e0c93faaf28eece6fd3516f2f449961d23ab4c94
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-10 11:22:56 +0200

    single item fetched, next is to  change to getData() from utils

--- js/details.js
+++ js/details.js
@@ -4,19 +4,16 @@ const querystring = document.location.search;
 const mySearchParams = new URLSearchParams(querystring);
 const id = mySearchParams.get('id');
 const newID = id.toString();
+console.log('id:', id);
 
-console.log(newID);
-
-// const url = `https://www.johann.one/wp-json/wc/v3/products?consumer_key=ck_665f152a7ef7923e561fd71862902f11f72672c9&consumer_secret=cs_bce68a8f771bf9355c3c48d304d3e50e530e2ae0&q=${id}`;
-// const url = `https://www.johann.one/wp-json/wc/v3/products?consumer_key=ck_665f152a7ef7923e561fd71862902f11f72672c9&consumer_secret=cs_bce68a8f771bf9355c3c48d304d3e50e530e2ae0&id=${id}`;
+const detailsItem = document.querySelector('.details-item');
 
 const getSingleData = () => {
   fetch(
-    `https://www.johann.one/wp-json/wc/v3/products?consumer_key=ck_665f152a7ef7923e561fd71862902f11f72672c9&consumer_secret=cs_bce68a8f771bf9355c3c48d304d3e50e530e2ae0&id=${id}`
+    `https://www.johann.one/wp-json/wc/v3/products/${id}?consumer_key=ck_665f152a7ef7923e561fd71862902f11f72672c9&consumer_secret=cs_bce68a8f771bf9355c3c48d304d3e50e530e2ae0`
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       displayOneObject(data);
     })
     .catch((e) =>
@@ -25,5 +22,13 @@ const getSingleData = () => {
 };
 getSingleData();
 const displayOneObject = (data) => {
-  console.log(data);
+  console.log(data.name);
+  detailsItem.innerHTML = `
+  <img src="${data.images[0].src}" alt="${data.images[0].alt}"/>
+  <p>${data.name}</p>
+  `;
 };
+
+// !mal
+// const url = `https://www.johann.one/wp-json/wc/v3/products?consumer_key=ck_665f152a7ef7923e561fd71862902f11f72672c9&consumer_secret=cs_bce68a8f771bf9355c3c48d304d3e50e530e2ae0&q=${id}`;
+// const url = `https://www.johann.one/wp-json/wc/v3/products?consumer_key=ck_665f152a7ef7923e561fd71862902f11f72672c9&consumer_secret=cs_bce68a8f771bf9355c3c48d304d3e50e530e2ae0&id=${id}`;
