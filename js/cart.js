4b3bf9e848c293efc27dfe7e5ce79fe61d402edb
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-10 16:30:31 +0200

    cart now has data, mostly styling needed on all pages

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,39 @@
+const cartDetails = document.querySelector('.cart-details');
+const cartItemsHTML = document.querySelector('.cart-itemsHTML');
+
 let totalItems = JSON.parse(localStorage.getItem('total'));
 let cart = JSON.parse(localStorage.getItem('cart'));
-
-let totalPricePerItem = 0;
 let totalPrice = 0;
-cart.map((item) => {
-  if (item.amountInCart > 0) {
-    totalPricePerItem = item.amountInCart * item.price;
-    console.log(totalPricePerItem);
-    totalPrice += totalPricePerItem;
-  }
-});
+// let itemsInCart = [];
 
-console.log(totalPrice);
+function getTotalValues() {
+  cart.map((item) => {
+    const { amountInCart, price, name, images } = item;
+    // console.log(images[0].src);
+    let totalPricePerItem = 0;
+    // cartDetails.innerHTML = '';
+    if (amountInCart > 0) {
+      totalPricePerItem = amountInCart * price;
+      totalPrice += totalPricePerItem;
+      const pricePerItems = `
+        <p>
+        <img src='${images[0].src}' />;
+        <strong>${name}</strong> Total price per item: <strong>
+            ${totalPricePerItem}
+        </strong>
+      </p>
+      `;
+
+      cartDetails.innerHTML += pricePerItems;
+    }
+  });
+  cartDetails.innerHTML += `
+          
+              <p>Total Items in Cart: <strong>${totalItems}</strong></p>
+              <p>Total Price: <strong>${totalPrice}</strong></p>
+      `;
+
+  console.log(totalItems);
+}
+
+getTotalValues();
